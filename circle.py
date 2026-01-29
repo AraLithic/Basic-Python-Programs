843ea3bd6320e653b6ef00e6e771503d17178b84
AraLithic <aralithic@gmail.com>
2026-01-30 03:29:16 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,5 @@
+import math
+radii =float(input("enter radius: "))
+area=math.pi*pow(radii,2)
+circumference=2*math.pi*radii
+print(f"area is {round(area,2)} meters^2 \ncircumference is{round(circumference,2)} meters")
