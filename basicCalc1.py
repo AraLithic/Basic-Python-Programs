4cc99c6c5a8f65c4be809dac47d1eb16426ae6b1
AraLithic <aralithic@gmail.com>
2026-01-30 03:31:08 +0530

    Add files via upload

---
+++ basicCalc1.py
@@ -0,0 +1,10 @@
+import math
+print("welcome to the calculator")
+a=float(input("enter the first number: "))
+b=float(input("enter the second number: "))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+mod=a%b
+print(f"addition is: {add}\nsubstraction is: {sub}\nmultiplication is: {mul}\ndivision is: {div}\nmodulus is: {mod}\nThank You")
