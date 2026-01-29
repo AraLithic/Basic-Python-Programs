0172a1b36e95b1192470d9a26c9b5740bce5315e
AraLithic <aralithic@gmail.com>
2026-01-30 03:28:17 +0530

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,15 @@
+friends=0
+friends=friends+1
+print(friends)
+friends=friends-2
+print(friends)
+friends=friends*-3
+print(friends)
+friends=friends/3
+print(friends)
+friends=12
+print(friends)
+friends=friends**2
+print(friends)
+friends= friends%2
+print(friends)
