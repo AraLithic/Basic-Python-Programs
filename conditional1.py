7b85da567b58a6f702ed3beabe0c5fb878df80e2
AraLithic <aralithic@gmail.com>
2026-01-30 03:30:33 +0530

    Add files via upload

---
+++ conditional1.py
@@ -0,0 +1,7 @@
+age=float(input("enter the age: "))
+if age>18:
+    print(f"you are old enough to vote")
+elif age==18:
+    print("see you a year later")
+else:
+    print("you are not old enough to vote")
